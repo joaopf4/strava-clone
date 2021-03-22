ac46d2595238e34b96c8226c9c56726bebb7402b
Joao Pedro <joaopfa@hotmail.com>
2021-03-22 19:05:34 -0300

    profile feed session

--- src/pages/feed/profile/styled.js
+++ src/pages/feed/profile/styled.js
@@ -6,4 +6,15 @@ export const ProfileFeed = styled.div`
     justify-content: flex-start;
     background-color: #f1f1f1;
     min-height: 86vh;
+    padding: 20px;
+    img {
+        border-radius: 50%;
+        width: 150px;
+    }
+    h1 {
+        font-size: 16px;
+        padding: 6px;
+        border-radius: 4px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
 `
